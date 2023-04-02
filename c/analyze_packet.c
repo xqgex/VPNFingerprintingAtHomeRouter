26279e2c5061e881d864fe6c62f81d1fdaa6acb4
xqgex <xqgex@users.noreply.github.com>
2023-04-02 00:49:21 -0400

    Convert to C version 1.03 (#15)

--- c/analyze_packet.c
+++ c/analyze_packet.c
@@ -1,18 +1,22 @@
-#include <linux/init.h> 
+#include <linux/init.h>
 #include <linux/kernel.h>
-#include <linux/module.h> 
-#include <linux/slab.h>
+#include <linux/module.h> /* MODULE_LICENSE() */
+#include <linux/slab.h> /* kmalloc(), kfree() */
+
+#include "analyze_packet.h"
+
+/**************************************************************************************************/
+/**** Macros                                                                                   ****/
+/**************************************************************************************************/
 
 #define RET_FAILURE 0U
 #define RET_SUCCESS 1U
 
-typedef unsigned int ip_type;
-typedef unsigned int timestamp_type;
-typedef unsigned int function_ret_type;
+/**************************************************************************************************/
+/**** Typedefs                                                                                 ****/
+/**************************************************************************************************/
 
-static unsigned int   const count_packets            = 10000U;
-static timestamp_type const time_window_sec          = 20U * 60U; /* 20 minutes */
-static timestamp_type const window_overlap_threshold = (timestamp_type)(0.75f * count_packets);
+typedef unsigned int function_ret_type;
 
 /**
  * A data structure for an internal endpoint, that is used to track
@@ -36,11 +40,32 @@ typedef struct hosts_node {
   struct hosts_node      *prev;
 } hosts_node_type;
 
-/* The head and tail of the list of endpoints in the network. */
-hosts_node_type *hosts_head = NULL;
-hosts_node_type *hosts_tail = NULL;
+/**************************************************************************************************/
+/**** Static variables                                                                         ****/
+/**************************************************************************************************/
 
-hosts_node_type* create_empty_node(ip_type value) {
+static unsigned int    const  count_packets            = METRIC_COUNT_PACKETS;
+static timestamp_type  const  time_window_sec          = METRIC_TIME_WINDOW_SEC;
+static timestamp_type  const  window_overlap_threshold = METRIC_WINDOW_OVERLAP_THRESHOLD;
+static hosts_node_type       *hosts_head               = NULL;
+static hosts_node_type       *hosts_tail               = NULL;
+
+/**************************************************************************************************/
+/**** Private functions prototype                                                              ****/
+/**************************************************************************************************/
+
+hosts_node_type   *create_empty_node(ip_type value);
+function_ret_type  insert_node(hosts_node_type *const new_node);
+function_ret_type  is_suspected_vpn(ip_type ip_source);
+void               remove_node(hosts_node_type *node_to_remove);
+void               report(ip_type ip_source, ip_type ip_destination, timestamp_type timestamp);
+hosts_node_type   *search_node(ip_type ip_source);
+
+/**************************************************************************************************/
+/**** Private functions                                                                        ****/
+/**************************************************************************************************/
+
+hosts_node_type *create_empty_node(ip_type value) {
   hosts_node_type *new_node = (hosts_node_type *)kmalloc(sizeof(hosts_node_type), GFP_KERNEL);
   new_node->ip                        = value;
   new_node->connection.ip_destination = 0U;
@@ -75,7 +100,7 @@ function_ret_type insert_node(hosts_node_type *const new_node) {
     new_node->prev = hosts_tail;
     hosts_tail = new_node;
   } else {
-    hosts_node_type* before_node = hosts_head;
+    hosts_node_type *before_node = hosts_head;
     while ((before_node->next != NULL) && (before_node->next->ip <= new_node->ip)) {
       before_node = before_node->next;
       /**
@@ -120,6 +145,24 @@ function_ret_type insert_node(hosts_node_type *const new_node) {
   return ret;
 }
 
+function_ret_type is_suspected_vpn(ip_type ip_source) {
+  function_ret_type  ret  = RET_FAILURE;
+  hosts_node_type   *node = search_node(ip_source);
+  /* Note, The `if` was broken into two "parts" just to increase the readability. */
+  if (node != NULL) {
+    if
+    (
+         (count_packets < node->connection.count_this)
+      || (window_overlap_threshold < (node->connection.count_prev + node->connection.count_this))
+    ) {
+      ret = RET_SUCCESS;
+    }
+  } else {
+    printk(KERN_INFO "[Error] Reporter could not find an IP that should exist `%d`\n", ip_source);
+  }
+  return ret;
+}
+
 void remove_node(hosts_node_type *node_to_remove) {
   if (node_to_remove != NULL) {
     if (node_to_remove == hosts_head) {
@@ -143,27 +186,6 @@ void remove_node(hosts_node_type *node_to_remove) {
   }
 }
 
-hosts_node_type *search_node(ip_type ip_source) {
-  hosts_node_type* loop_node = hosts_head;
-  while ((loop_node != NULL) && (loop_node->ip < ip_source)) {
-    loop_node = loop_node->next;
-  }
-  if ((loop_node != NULL) && (ip_source != loop_node->ip)) {
-    loop_node = NULL;
-  }
-  return loop_node;
-}
-
-void debug_print_all_hosts(void) {
-  hosts_node_type* loop_node = hosts_head;
-  printk(KERN_INFO "List of hosts: [");
-  while (loop_node != NULL) {
-    printk(KERN_CONT "%d, ", loop_node->ip);
-    loop_node = loop_node->next;
-  }
-  printk(KERN_CONT "NULL]\n");
-}
-
 void report(ip_type ip_source, ip_type ip_destination, timestamp_type timestamp) {
   (void)ip_source;
   (void)ip_destination;
@@ -177,27 +199,24 @@ void report(ip_type ip_source, ip_type ip_destination, timestamp_type timestamp)
   );
 }
 
-function_ret_type is_suspected_vpn(ip_type ip_source) {
-  function_ret_type ret = RET_FAILURE;
-  hosts_node_type *node = search_node(ip_source);
-  /* Note, The `if` was broken into two "parts" just to increase the readability. */
-  if (node != NULL) {
-    if
-    (
-         (count_packets < node->connection.count_this)
-      || (window_overlap_threshold < (node->connection.count_prev + node->connection.count_this))
-    ) {
-      ret = RET_SUCCESS;
-    }
-  } else {
-    printk(KERN_INFO "[Error] Reporter could not find an IP that should exist `%d`\n", ip_source);
+hosts_node_type *search_node(ip_type ip_source) {
+  hosts_node_type *loop_node = hosts_head;
+  while ((loop_node != NULL) && (loop_node->ip < ip_source)) {
+    loop_node = loop_node->next;
   }
-  return ret;
+  if ((loop_node != NULL) && (ip_source != loop_node->ip)) {
+    loop_node = NULL;
+  }
+  return loop_node;
 }
 
+/**************************************************************************************************/
+/**** Public functions                                                                         ****/
+/**************************************************************************************************/
+
 void analyze(ip_type ip_source, ip_type ip_destination, timestamp_type timestamp) {
-  function_ret_type ret = RET_SUCCESS;
-  hosts_node_type *node = search_node(ip_source);
+  function_ret_type  ret  = RET_SUCCESS;
+  hosts_node_type   *node = search_node(ip_source);
   if (node == NULL) { /* Initial record */
     node = create_empty_node(ip_source);
     ret = insert_node(node);
@@ -214,7 +233,7 @@ void analyze(ip_type ip_source, ip_type ip_destination, timestamp_type timestamp
       if (RET_SUCCESS == is_suspected_vpn(ip_source)) {
         report(ip_source, ip_destination, timestamp);
       }
-      node->connection.timestamp = timestamp;
+      node->connection.timestamp  = timestamp;
       node->connection.count_prev = node->connection.count_this;
       node->connection.count_this = 0;
     }
@@ -223,50 +242,14 @@ void analyze(ip_type ip_source, ip_type ip_destination, timestamp_type timestamp
   }
 }
 
-static int __init hello_init(void) {
-  printk(KERN_INFO "Hello, world\n");
-  /* Initial */
-  printk(KERN_INFO "Initial list\n");
-  debug_print_all_hosts();
-  /* Insert 4 numbers */
-  printk(KERN_INFO "Insert `8`, `1000`, `30` and then `2`\n");
-  analyze(8, 0, 0);
-  analyze(1000, 0, 0);
-  analyze(30, 0, 0);
-  analyze(2, 0, 0);
-  debug_print_all_hosts();
-  /* Remove tail */
-  printk(KERN_INFO "Remove the tail\n");
-  remove_node(hosts_tail);
-  debug_print_all_hosts();
-  /* Remove head */
-  printk(KERN_INFO "Remove the head\n");
-  remove_node(hosts_head);
-  debug_print_all_hosts();
-  /* Insert and then remove */
-  printk(KERN_INFO "Insert `15`\n");
-  analyze(15, 0, 0);
-  debug_print_all_hosts();
-  printk(KERN_INFO "Remove `15`\n");
-  remove_node(search_node(15));
-  debug_print_all_hosts();
-  printk(KERN_INFO "Check the reporter for IP `8`\n");
-  printk(KERN_INFO "Second packet from `8`\n");
-  analyze(8, 0, 0);
-  printk(KERN_INFO "Third packet from `8`\n");
-  analyze(8, 0, 0);
-  printk(KERN_INFO "Fourth packet from `8`\n");
-  analyze(8, 0, 0);
-  /* Done */
-  printk(KERN_INFO "Done\n");
-  return 0;
+void debug_print_all_hosts(void) {
+  hosts_node_type const *loop_node = hosts_head;
+  printk(KERN_INFO "List of hosts: [");
+  while (loop_node != NULL) {
+    printk(KERN_CONT "%d, ", loop_node->ip);
+    loop_node = loop_node->next;
+  }
+  printk(KERN_CONT "NULL]\n");
 }
-
-static void __exit hello_exit(void) {
-  printk(KERN_INFO "Goodbye, world\n");
-}
-
-module_init(hello_init);
-module_exit(hello_exit);
 
 MODULE_LICENSE("GPL");
---
+++ c/analyze_packet.h
@@ -0,0 +1,26 @@
+#ifndef ANALYZE_PACKET_H
+#define ANALYZE_PACKET_H
+
+/**************************************************************************************************/
+/**** Macros                                                                                   ****/
+/**************************************************************************************************/
+
+#define METRIC_COUNT_PACKETS            10000U
+#define METRIC_TIME_WINDOW_SEC          20U * 60U /* 20 minutes */
+#define METRIC_WINDOW_OVERLAP_THRESHOLD (timestamp_type)(0.75f * (METRIC_COUNT_PACKETS))
+
+/**************************************************************************************************/
+/**** Typedefs                                                                                 ****/
+/**************************************************************************************************/
+
+typedef unsigned int ip_type;
+typedef unsigned int timestamp_type;
+
+/**************************************************************************************************/
+/**** Functions prototype                                                                      ****/
+/**************************************************************************************************/
+
+void analyze(ip_type ip_source, ip_type ip_destination, timestamp_type timestamp);
+void debug_print_all_hosts(void);
+
+#endif /* ANALYZE_PACKET_H */
