49ae3c389d313e0c2658a49f3a60a8d1df1dc20c
Yogev <xqgex.n@gmail.com>
2023-04-01 22:05:20 -0400

    Convert to C version 1.02

--- c/vpn_fingerprinting.c
+++ c/vpn_fingerprinting.c
@@ -6,19 +6,23 @@
 #define RET_FAILURE 0U
 #define RET_SUCCESS 1U
 
-static unsigned int const count_packets            = 10000U;
-static unsigned int const time_window_sec          = 20 * 60U; /* 20 minutes */
-static float        const window_overlap_threshold = 0.75f * count_packets;
+typedef unsigned int ip_type;
+typedef unsigned int timestamp_type;
+typedef unsigned int function_ret_type;
+
+static unsigned int   const count_packets            = 10000U;
+static timestamp_type const time_window_sec          = 20U * 60U; /* 20 minutes */
+static timestamp_type const window_overlap_threshold = (timestamp_type)(0.75f * count_packets);
 
 /**
  * A data structure for an internal endpoint, that is used to track
  * the latest connection the endpoint have with the "outside world".
  */
 typedef struct TrackedConnection {
-  unsigned int ip_destination;
-  float        timestamp;
-  unsigned int count_this_window;
-  unsigned int count_prev_window;
+  ip_type        ip_destination;
+  timestamp_type timestamp;
+  unsigned int   count_this;
+  unsigned int   count_prev;
 } TrackedConnection_type;
 
 /**
@@ -26,7 +30,7 @@ typedef struct TrackedConnection {
  * The endpoints are stored in a sorted doubly linked list.
  */
 typedef struct hosts_node {
-  unsigned int            ip;
+  ip_type                 ip;
   TrackedConnection_type  connection;
   struct hosts_node      *next;
   struct hosts_node      *prev;
@@ -36,20 +40,20 @@ typedef struct hosts_node {
 hosts_node_type *hosts_head = NULL;
 hosts_node_type *hosts_tail = NULL;
 
-hosts_node_type* create_empty_node(unsigned int value) {
+hosts_node_type* create_empty_node(ip_type value) {
   hosts_node_type *new_node = (hosts_node_type *)kmalloc(sizeof(hosts_node_type), GFP_KERNEL);
-  new_node->ip                           = value;
-  new_node->connection.ip_destination    = 0U;
-  new_node->connection.timestamp         = 0.0f;
-  new_node->connection.count_this_window = 0U;
-  new_node->connection.count_prev_window = 0U;
-  new_node->prev                         = NULL;
-  new_node->next                         = NULL;
+  new_node->ip                        = value;
+  new_node->connection.ip_destination = 0U;
+  new_node->connection.timestamp      = 0U;
+  new_node->connection.count_this     = 0U;
+  new_node->connection.count_prev     = 0U;
+  new_node->prev                      = NULL;
+  new_node->next                      = NULL;
   return new_node;
 }
 
-unsigned int insert_node(hosts_node_type *const new_node) {
-  unsigned int ret = RET_SUCCESS;
+function_ret_type insert_node(hosts_node_type *const new_node) {
+  function_ret_type ret = RET_SUCCESS;
   if (NULL == new_node) {
     /* To be safe */
     ret = RET_FAILURE;
@@ -81,7 +85,7 @@ unsigned int insert_node(hosts_node_type *const new_node) {
        * Code for example:
        * ```C
        * if (0 == SOMETHING_WAS_REMOVED) {
-       *   if (CURRENT_TIMESTAMP - before_node->prev.connection.timestamp < CONSTANT_VALUE) {
+       *   if (CURRENT_TIMESTAMP - before_node->prev->connection.timestamp < CONSTANT_VALUE) {
        *     remove_node(before_node->prev);
        *     SOMETHING_WAS_REMOVED = 1;
        *   }
@@ -117,13 +121,13 @@ unsigned int insert_node(hosts_node_type *const new_node) {
 }
 
 void remove_node(hosts_node_type *node_to_remove) {
-  if (NULL != node_to_remove) {
-    if (hosts_head == node_to_remove) {
+  if (node_to_remove != NULL) {
+    if (node_to_remove == hosts_head) {
       if (hosts_head->next != NULL) {
         hosts_head->next->prev = NULL;
       }
       hosts_head = hosts_head->next;
-    } else if (hosts_tail == node_to_remove) {
+    } else if (node_to_remove == hosts_tail) {
       if (hosts_tail->prev != NULL) { /* Just to be safe */
         hosts_tail->prev->next = NULL;
       }
@@ -139,6 +143,17 @@ void remove_node(hosts_node_type *node_to_remove) {
   }
 }
 
+hosts_node_type *search_node(ip_type ip_source) {
+  hosts_node_type* loop_node = hosts_head;
+  while ((loop_node != NULL) && (loop_node->ip < ip_source)) {
+    loop_node = loop_node->next;
+  }
+  if ((loop_node != NULL) && (ip_source != loop_node->ip)) {
+    loop_node = NULL;
+  }
+  return loop_node;
+}
+
 void debug_print_all_hosts(void) {
   hosts_node_type* loop_node = hosts_head;
   printk(KERN_INFO "List of hosts: [");
@@ -149,32 +164,76 @@ void debug_print_all_hosts(void) {
   printk(KERN_CONT "NULL]\n");
 }
 
-void report(unsigned int ip_source, unsigned int ip_destination, float timestamp) {
+void report(ip_type ip_source, ip_type ip_destination, timestamp_type timestamp) {
   (void)ip_source;
   (void)ip_destination;
   (void)timestamp;
+  printk
+  (
+    KERN_INFO "[Reporter] source `%d` destination `%d` timestamp `%d`\n",
+    ip_source,
+    ip_destination,
+    timestamp
+  );
+}
+
+function_ret_type is_suspected_vpn(ip_type ip_source) {
+  function_ret_type ret = RET_FAILURE;
+  hosts_node_type *node = search_node(ip_source);
+  /* Note, The `if` was broken into two "parts" just to increase the readability. */
+  if (node != NULL) {
+    if
+    (
+         (count_packets < node->connection.count_this)
+      || (window_overlap_threshold < (node->connection.count_prev + node->connection.count_this))
+    ) {
+      ret = RET_SUCCESS;
+    }
+  } else {
+    printk(KERN_INFO "[Error] Reporter could not find an IP that should exist `%d`\n", ip_source);
+  }
+  return ret;
+}
+
+void analyze(ip_type ip_source, ip_type ip_destination, timestamp_type timestamp) {
+  function_ret_type ret = RET_SUCCESS;
+  hosts_node_type *node = search_node(ip_source);
+  if (node == NULL) { /* Initial record */
+    node = create_empty_node(ip_source);
+    ret = insert_node(node);
+  }
+  if (RET_SUCCESS == ret) {
+    if (ip_destination != node->connection.ip_destination) { /* New connection */
+      node->connection.ip_destination = ip_destination;
+      node->connection.timestamp      = timestamp;
+      node->connection.count_this     = 1U;
+    } else {
+      node->connection.count_this += 1;
+    }
+    if (time_window_sec < timestamp - node->connection.timestamp) { /* Start a new window */
+      if (RET_SUCCESS == is_suspected_vpn(ip_source)) {
+        report(ip_source, ip_destination, timestamp);
+      }
+      node->connection.timestamp = timestamp;
+      node->connection.count_prev = node->connection.count_this;
+      node->connection.count_this = 0;
+    }
+  } else {
+    printk(KERN_INFO "[Error] Failed to insert a new node with IP `%d`\n", ip_source);
+  }
 }
 
 static int __init hello_init(void) {
-  hosts_node_type *new_node;
   printk(KERN_INFO "Hello, world\n");
   /* Initial */
   printk(KERN_INFO "Initial list\n");
   debug_print_all_hosts();
   /* Insert 4 numbers */
   printk(KERN_INFO "Insert `8`, `1000`, `30` and then `2`\n");
-  if (RET_FAILURE == insert_node(create_empty_node(8))) {
-    printk(KERN_INFO "[Error] Failed to insert the value `8`\n");
-  }
-  if (RET_FAILURE == insert_node(create_empty_node(1000))) {
-    printk(KERN_INFO "[Error] Failed to insert the value `1000`\n");
-  }
-  if (RET_FAILURE == insert_node(create_empty_node(30))) {
-    printk(KERN_INFO "[Error] Failed to insert the value `30`\n");
-  }
-  if (RET_FAILURE == insert_node(create_empty_node(2))) {
-    printk(KERN_INFO "[Error] Failed to insert the value `2`\n");
-  }
+  analyze(8, 0, 0);
+  analyze(1000, 0, 0);
+  analyze(30, 0, 0);
+  analyze(2, 0, 0);
   debug_print_all_hosts();
   /* Remove tail */
   printk(KERN_INFO "Remove the tail\n");
@@ -186,14 +245,18 @@ static int __init hello_init(void) {
   debug_print_all_hosts();
   /* Insert and then remove */
   printk(KERN_INFO "Insert `15`\n");
-  new_node = create_empty_node(15);
-  if (RET_FAILURE == insert_node(new_node)) {
-    printk(KERN_INFO "[Error] Failed to insert the value `15`\n");
-  }
+  analyze(15, 0, 0);
   debug_print_all_hosts();
   printk(KERN_INFO "Remove `15`\n");
-  remove_node(new_node);
+  remove_node(search_node(15));
   debug_print_all_hosts();
+  printk(KERN_INFO "Check the reporter for IP `8`\n");
+  printk(KERN_INFO "Second packet from `8`\n");
+  analyze(8, 0, 0);
+  printk(KERN_INFO "Third packet from `8`\n");
+  analyze(8, 0, 0);
+  printk(KERN_INFO "Fourth packet from `8`\n");
+  analyze(8, 0, 0);
   /* Done */
   printk(KERN_INFO "Done\n");
   return 0;
