37cad51ec1bd6bc543293277eb432e251ba5f50e
Yogev <xqgex.n@gmail.com>
2023-04-01 21:59:16 -0400

    Convert to C version 1.00

---
+++ c/vpn_fingerprinting.c
@@ -0,0 +1,17 @@
+#include <linux/init.h> 
+#include <linux/kernel.h>
+#include <linux/module.h> 
+
+static int __init hello_init(void) {
+    printk(KERN_INFO "Hello, world\n");
+    return 0;
+}
+
+static void __exit hello_exit(void) {
+    printk(KERN_INFO "Goodbye, world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
